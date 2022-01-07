c03e3cc0278f54e38ff6cf3c818f2550db7a14a2
LimYunhwi <88yhtserof@naver.com>
2022-01-07 20:28:47 +0900

    계산기 기능 구현하기 -2
    1. 오타 수정
    2. 연산 함수 생성
    3. 연산자별 해당하는 연산자를 파라미터로 하여 연산 함수 호출
    4. 연산 기능 구현

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,7 +14,7 @@ enum Operation {
     case Subtract
     case Divide
     case Multiply
-    case unown //어떤 연산자도 해당하지 않을 때
+    case unknown //어떤 연산자도 해당하지 않을 때
 }
 
 class ViewController: UIViewController {
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
     var firstOperand = "" //이전 계산값을 저장하는 프로퍼티. 첫 번째 피연산자.
     var secondOperand = "" //새롭게 입력된 값을 저장하는 프로퍼티. 두 번째 피연산자.
     var result = "" // 계산의 결과값 할당
-    var currentOperation: Operation = .unown //현재 어떤 연산자가 입력되었는지 알 수 있게 연산자의 값을 저장하는 프로퍼티
+    var currentOperation: Operation = .unknown //현재 어떤 연산자가 입력되었는지 알 수 있게 연산자의 값을 저장하는 프로퍼티
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,7 +46,7 @@ class ViewController: UIViewController {
         self.firstOperand = ""
         self.secondOperand = ""
         self.result = ""
-        self.currentOperation = .unown
+        self.currentOperation = .unknown
         self.lblNumberOutput.text = "0"
     }
     
@@ -59,18 +59,68 @@ class ViewController: UIViewController {
     }
     
     @IBAction func tapBtnDivide(_ sender: UIButton) {
+        self.operation(.Divide)
     }
     
     @IBAction func tapBtnMultiply(_ sender: UIButton) {
+        self.operation(.Multiply)
     }
     
     @IBAction func tapBtnSubtract(_ sender: UIButton) {
+        self.operation(.Subtract)
     }
     
     @IBAction func tapBtnAdd(_ sender: UIButton) {
+        self.operation(.Add)
     }
     
     @IBAction func tapBtnEqual(_ sender: UIButton) {
+        self.operation(self.currentOperation)
+    }
+    
+    func operation(_ operation: Operation) {
+        //파라미터로 전달받은 연산자에 따라 계산. 계산된 결과값을 화면에 표시
+        if self.currentOperation != .unknown {
+            if !self.displayNumber.isEmpty {
+                self.secondOperand = self.displayNumber
+                self.displayNumber = "" //다음 피연산자를 받기 위함
+                
+                //피연산자 형변환
+                guard let firstOperand = Double(self.firstOperand) else {return}
+                guard let secondOperand = Double(self.secondOperand) else {return}
+                
+                //연산자에 따라 첫 번째 피연산자와 두 번째 피연산자 계산
+                switch currentOperation {
+                case .Add:
+                    self.result = "\(firstOperand + secondOperand)"
+                case .Subtract:
+                    self.result = "\(firstOperand - secondOperand)"
+                case .Divide:
+                    self.result = "\(firstOperand / secondOperand)"
+                case .Multiply:
+                    self.result = "\(firstOperand * secondOperand)"
+                default:
+                    break
+                }
+                
+                //소수점 아래의 값이 없다면 정수로 형변환
+                if let result = Double(self.result), result.truncatingRemainder(dividingBy: 1) == 0 {
+                    self.result = "\(Int(result))"
+                }
+                
+                //한 번의 연산이 끝나면, 그 결과값과 다음 피연산자를 다음 연산자를 가지고 계산해야 한다.
+                //따라서 첫 번째 연산자 프로퍼티에 결과값을 할당한다.
+                self.firstOperand = self.result
+                self.lblNumberOutput.text = self.result
+            }
+            self.currentOperation = operation //만약 1+2-3 이라면 1+2 연산을 한 후 currentOperation에 .Subtract 할당
+            
+        } else{//.unknown : 값이 초기화된 상태에서 사용자가 첫 번째 피연산자와 연산자를 입력한 상태
+            //만약 1+이라면 firstOperand에 1할당, currentOperation에 .Add 할당.
+            self.firstOperand = self.displayNumber // 화면에 표시된 피연산자.
+            self.currentOperation = operation //선택한 연산자
+            self.displayNumber = "" // 첫 번째 피연산자를 해당 프로퍼티에 할당했으므로 초기화. 두 번째 피연산자를 받기 위함
+        }
     }
 }
 
